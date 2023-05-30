8d33cbc0f3b71343ce4322c7c1bcd49960e89307
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-05-30 11:24:06 +0200

    Add sonar report byte for distances >2.5m

--- tmx_pico_aio/tmx_pico_aio.py
+++ tmx_pico_aio/tmx_pico_aio.py
@@ -1720,11 +1720,11 @@ class TmxPicoAio:
         cb = self.sonar_callbacks[report[0]]
 
         # build report data
-        if report[1] == 0 and report[2] == 0:
+        if report[1] == 0 and report[2] == 0 and report[3] == 0:
             cb_list = [PrivateConstants.SONAR_DISTANCE, report[0],
                        0, time.time()]
         else:
             cb_list = [PrivateConstants.SONAR_DISTANCE, report[0],
-                       (report[1] + (report[2] / 100)), time.time()]
+                       (report[1]*100 + report[2] + (report[3] / 100)), time.time()]
 
         await cb(cb_list)
